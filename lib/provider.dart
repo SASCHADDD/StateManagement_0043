e53c78f2c1ffddd9c9ba706c05e47c917fa6e192
SASCHADDD <saschadanu23@gmail.com>
2026-04-04 01:44:21 +0700

    membaut state mangament provider

--- lib/provider.dart
+++ lib/provider.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/setstate/setstate.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => FormProvider(),
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FormPage(),
+    );
+  }
+}
 
 class FormProvider extends ChangeNotifier {
   String nama = "";
