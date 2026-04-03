d673717fc67fea30666f58b6dd56520914573cfb
SASCHADDD <saschadanu23@gmail.com>
2026-04-04 01:51:24 +0700

    state provider

---
+++ lib/riverpod.dart
@@ -0,0 +1,16 @@
+//PROVIDER
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
+import 'package:state_management/main.dart';
+
+final namaProvider = StateProvider<String>((ref) => "");
+final emailProvider = StateProvider<String>((ref) => "");
+
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
