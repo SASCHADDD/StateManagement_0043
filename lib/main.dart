9a6a1286f861a7b0aa08d992598ad3a2e31d60ba
SASCHADDD <saschadanu23@gmail.com>
2026-04-04 01:54:47 +0700

    state bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:state_management/bloc/blocku.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    BlocProvider(
+      create: (context) => FormBloc(),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
