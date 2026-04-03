e0712c013ec2d2273d19cc08fe8a08c796811d32
SASCHADDD <saschadanu23@gmail.com>
2026-04-04 01:52:47 +0700

    widget untuk aplikasi

--- lib/riverpod.dart
+++ lib/riverpod.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:state_management/main.dart';
+import 'package:state_management/setstate/setstate.dart';
 
 final namaProvider = StateProvider<String>((ref) => "");
 final emailProvider = StateProvider<String>((ref) => "");
@@ -13,4 +14,15 @@ void main() {
       child: MyApp(),
     ),
   );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FormPage(),
+    );
+  }
 }
