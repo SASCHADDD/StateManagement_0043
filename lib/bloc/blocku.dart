44081baf061b852b34ad7c5a7e09e0765da66ac8
SASCHADDD <saschadanu23@gmail.com>
2026-04-04 01:17:58 +0700

    membuat class

---
+++ lib/bloc/blocku.dart
@@ -0,0 +1,11 @@
+abstract class FormEvent {}
+
+class NamaChanged extends FormEvent {
+  final String nama;
+  NamaChanged(this.nama);
+}
+
+class EmailChanged extends FormEvent {
+  final String email;
+  EmailChanged(this.email);
+}
